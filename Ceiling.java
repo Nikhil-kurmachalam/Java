d113064f87c790243426ab67d129f731bc8a7b47
Nikhil-kurmachalam <45930934+Nikhil-kurmachalam@users.noreply.github.com>
2023-02-15 11:09:29 +0530

    Create Ceiling.java

---
+++ Ceiling.java
@@ -0,0 +1,38 @@
+public class BinarySearch
+
+{
+    public static void main(String[] args) {
+ int arr[] = {1,2,3,7,8,9,21,33,43,654,5432,54322};
+ int target= 42;
+ System.out.println("Element found at: "+Bsearch(arr, target));
+    }
+
+
+    // return index
+    static int Bsearch(int[] arr, int target)
+    {
+        int start = 0;
+        int end = arr.length-1;
+
+        while(start<=end)
+        {
+            //find the element
+//            int mid = (start+end)/2; might be possible that start+end exceeds range of integer
+            int mid = start +(end-start)/2;
+
+            if(target<arr[mid])
+                end = mid-1;
+                        else if(target>arr[mid])
+                            start = mid+1;
+                        else
+            {
+                return mid;
+            }
+
+
+        }
+        return -1;
+
+    }
+
+}
