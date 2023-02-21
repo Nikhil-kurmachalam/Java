3d8b598a6233dcfdf8f6bbd128e22f92b93816e7
Nikhil-kurmachalam <45930934+Nikhil-kurmachalam@users.noreply.github.com>
2023-02-21 17:44:49 +0530

    Create Pattern1.java

---
+++ Patterns/Pattern1.java
@@ -0,0 +1,30 @@
+public class Main {
+    public static void main(String[] args)
+    {
+        int n= 9;
+        PrintPattern(n);
+    }
+    static void PrintPattern(int n)
+    {
+        for(int row=1;row<=n;row++)
+        {
+            for(int col=1;col<=row;col++) {
+                System.out.print("*");
+            }
+            System.out.print("\n");
+
+        }
+    }
+
+}
+//
+//output:
+//        *
+//        **
+//        ***
+//        ****
+//        *****
+//        ******
+//        *******
+//        ********
+//        *********
