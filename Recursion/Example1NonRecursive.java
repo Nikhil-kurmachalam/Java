1d47effafa4f4b4e3c2c23049646b0ad81335323
Nikhil-kurmachalam <45930934+Nikhil-kurmachalam@users.noreply.github.com>
2023-02-23 23:55:17 +0530

    Create Example1NonRecursive.java

---
+++ Recursion/Example1NonRecursive.java
@@ -0,0 +1,28 @@
+public class Main {
+    public static void main(String[] args)
+    {
+        int n = 1;
+        print1(1);
+    }
+
+    static void print1(int n)
+    {
+        System.out.println(n);
+        print2(2);
+    }
+    static void print2(int n)
+    {
+        System.out.println(n);
+        print3(3);
+    }
+    static void print3(int n)
+    {
+        System.out.println(n);
+        print4(4);
+    }
+    static void print4(int n)
+    {
+        System.out.println(n);
+    }
+// Find the file name: Example1RecursiveWay.java in same folder which was solved using recursion.
+}
